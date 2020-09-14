3a9c06d7ecf2a97f8f9d78cf2299e9002a8db8c0
lukem1 <37024616+lukem1@users.noreply.github.com>
2020-09-14 18:39:22 -0400

    Finished myList.py -Added ls output and links -Improved formatting

--- Deliverables/myList/myList.py
+++ Deliverables/myList/myList.py
@@ -22,32 +22,62 @@ htmlHead = """
 <title>Current Directory</title>
   <style>
   
-    body {   
-      background-color: rgb(150, 108, 39);    
-    }
+	body {   
+		background-color: rgb(150, 108, 39);
+	}
     
-    h1 {
-      display: flex;
-      justify-content: center;
-      color: rgb(255, 200, 53);
-    }
+	h1 {
+		display: flex;
+		justify-content: center;
+		color: rgb(255, 200, 53);
+	}
     
-    .scripts {
-      display: flex;
-      justify-content: center;
-      color: rgb(243, 242, 236);
-    }
+	.scripts {
+		display: flex;
+		justify-content: center;
+		color: rgb(243, 242, 236);
+	}
     
-    p {
-      display: flex;
-      text-align: left;
-    }
+	p {
+		display: flex;
+		text-align: left;
+		justify-content: center;
+	}
+    
+    .button
+	{
+		border: none;
+		border-radius: 12px;
+		color: white;
+		padding: 8px 16px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 12px;
+	}
+
+	.button:hover
+	{
+		box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+	}
+	.wrapper
+	{
+		padding-bottom: 10px;
+		text-align: center;
+		justify-content: center;
+	}
+
     
   </style>
 
 </head>
 """
 
+button = """
+<form action="file://%s" style="display: inline-block;">
+	<button class="button" type="submit" style="background-color: #ff4015">Go</button>
+</form>
+"""
 # Build the html file from command output, store in /tmp, and open in web browser
 def buildHTML(lsOut, treeOut):
 	tmpFile = "/tmp/newleaf-myList.html"
@@ -57,10 +87,18 @@ def buildHTML(lsOut, treeOut):
 	with open(tmpFile, "w+") as file:
 		file.write(htmlHead)
 		file.write("<body>\n<h1>%s</h1>" % startDir)
-		file.write("<div class='scripts'>")
+		#file.write("<div class='scripts'>")
 		#TODO: Parse lsOut and create tags for text, buttons
+		ls = lsOut.split("\n")[1:-1]
+		for f in ls:
+			path = "%s/%s" % (startDir, f.split(" ")[-1])
+			file.write("<div class='wrapper'><p style='display: inline-block;'>%s</p>" % f)
+			file.write(button % path)
+			file.write("</div>")
+			
+		file.write("<p style='color: rgb(255, 200, 53);'>Directory Tree</p>")
 		file.write("<p>%s</p>" % treeOut.replace("\n", "<br>"))
-		file.write("</div>\n</body>")
+		file.write("</body>")
 		
 		
 	print("Opening %s..." % tmpFile)
@@ -76,7 +114,7 @@ def main():
 	# Run tree command if available and capture output
 	treeOut = "Install tree for even more information!"
 	try:
-		tree = run(["tree"], capture_output=True, text=True)
+		tree = run(["nmap"], capture_output=True, text=True)
 		treeOut = tree.stdout
 	except FileNotFoundError:
 		print("tree not found, install with: sudo apt install tree")
