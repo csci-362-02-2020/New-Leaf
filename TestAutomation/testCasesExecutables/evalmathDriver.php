8782ac7f2be187478858137fef2a8a9a9b5db2b8
lukem1 <37024616+lukem1@users.noreply.github.com>
2020-10-20 02:52:55 -0400

    Created evalmathDriver, updated testCases

---
+++ TestAutomation/testCasesExecutables/evalmathDriver.php
@@ -0,0 +1,50 @@
+<?php
+
+//
+// evalmathDriver.php
+//
+// lukem1
+// Newleaf
+// 19 October 2020
+//
+
+
+// Requirements
+// Note: Not part of moodle_internal, so no need to load moodle config
+require "../../../lib/evalmath/evalmath.class.php";
+
+
+// Input expression and expected output must be passed as command line arguments
+// Warning: Input must not include spaces, if spaces are required this must be rewritten
+if (!(sizeof($argv) == 3)) { 
+	echo "Error: evalmathDriver requires sizeof(\$argv) == 3 \n";	
+	return -1; 
+}
+
+
+// Method to perform a test
+// Note: Output here should be parseable, may need to be changed
+function test($input, $expected) {
+	echo "Running evalmath Test \n";
+	echo "Input: " . $input . "\n";
+	echo "Expected Output: " . $expected . "\n";
+	
+	$math = new EvalMath;
+	$result = $math->evaluate($input);
+	
+	echo $input . " = " . $expected . " -> ";
+	if ($result == $expected) {
+		echo "Test Passed.\n";
+	}
+	else {
+		echo "Test Failed.\n";
+	}
+}
+
+
+// Run the test with command line input
+$in = $argv[1];
+$expected = (int) $argv[2];
+
+test($in, $expected);
+
